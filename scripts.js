12d9cf43aad0179166fce69fdb0cb8b6bdfb333e
kenshi <samuduchanuka@gmail.com>
2024-02-11 11:00:51 +0530

    Add functionality for removing a book and toggling the value of whether a book has been read or not

--- scripts.js
+++ scripts.js
@@ -1,30 +1,21 @@
-// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
-
 const container = document.querySelector('.container');
-const btnAddBook = document.querySelector('.addBookBtn');
 const dialog = document.querySelector('dialog');
-const closeButton = document.querySelector("form + button");
-const form = document.querySelector('form');
-// let btnRemove;
+const formAddBook = document.querySelector('form');
+
+let btnRemove;
+let btnRead;
 
 const myLibrary = [];
 
+// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
 book_1 = new Book('Little Women', 'Louisa May Alcott', 528, 'Not Read');
 book_2 = new Book('The Book Thief', 'Markus Zusak', 592, 'Not Read');
 book_3 = new Book('The Hunger Games', 'Suzanne Collins', 374, 'Not Read');
 book_4 = new Book('1984', 'George Orwell', 368, 'Not Read');
 book_5 = new Book('To Kill a Mockingbird', 'Harper Lee', 323, 'Not Read');
 
-addBookToLibrary(book_1)
-addBookToLibrary(book_2)
-
-btnAddBook.addEventListener('click', () => {
-    dialog.showModal();
-});
-
-closeButton.addEventListener('click', () => {
-    dialog.close()
-});
+myLibrary.push(book_1)
+myLibrary.push(book_2)
 
 function Book(title, author, numOfPages, isRead){
     this.title = title;
@@ -37,13 +28,13 @@ function Book(title, author, numOfPages, isRead){
     }
 }
 
-form.addEventListener("submit", (event) => {
+formAddBook.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
-    const numPages = document.querySelector('#numPages').value;
-    let isRead = document.querySelector('#isRead').value;
+    const numPages = document.querySelector('#num-pages').value;
+    let isRead = document.querySelector('#is-read').value;
 
     if (isRead === 'no') {
         isRead = 'Not Read'
@@ -62,14 +53,10 @@ form.addEventListener("submit", (event) => {
     dialog.close();
 })
 
-function addBookToLibrary(book){
-    myLibrary.push(book);
-}
-
 function displayLibrary(startIndex = 0){
     for (let i = startIndex; i < myLibrary.length; i++){
         let card = document.createElement('div');
-        card.classList.add('card');
+        card.classList.add('card', `index_${i}`);
         let title = document.createElement('div');
         title.textContent = myLibrary[i].title;
         title.classList.add('title');
@@ -80,39 +67,68 @@ function displayLibrary(startIndex = 0){
         
         let numPages = document.createElement('div');
         numPages.textContent = myLibrary[i].numOfPages;
-        numPages.classList.add('numPages');
+        numPages.classList.add('num-pages');
         
         let isRead = document.createElement('div');
         isRead.textContent = myLibrary[i].isRead;
-        isRead.classList.add('isRead');
+        isRead.classList.add('is-read');
 
         let options = document.createElement('div');
         options.classList.add('options');
 
-        // let removeBook = document.createElement('button');
-        // removeBook.classList.add('removeBook');
-        // removeBook.textContent = 'Remove';
+        let removeBook = document.createElement('button');
+        removeBook.classList.add('removeBook');
+        removeBook.textContent = 'Remove';
 
-        // let read = document.createElement('button');
-        // read.classList.add('read');
-        // read.textContent = 'Read';
+        let read = document.createElement('button');
+        read.classList.add('btn-read');
+        read.textContent = 'Read';
         
         container.appendChild(card);
-        card.append(title, author, numPages, isRead);
 
-        // card.append(title, author, numPages, isRead, options);
-        // options.append(removeBook, read)
-
-        // btnRemove = document.querySelectorAll('.removeBook');
-
-        // btnRemove.forEach((value) => {
-        //     value.addEventListener("click", (event) => {
-        //         event.target.parentElement.parentElement.remove()
-        //         console.log(myLibrary)
-        //     })
-        // })
+        card.append(title, author, numPages, isRead, options);
+        options.append(removeBook, read)
     }
+    
+    btnRemove = document.querySelectorAll('.removeBook');
+    addRemoveBook(btnRemove);
+
+    btnRead = document.querySelectorAll('.btn-read');
+    addToggleRead(btnRead);
 }
 
 displayLibrary();
 
+function addRemoveBook(btnRemove) {
+    btnRemove.forEach((value) => {
+        value.addEventListener("click", (event) => {
+            element = event.target.parentElement.parentElement
+            removeBook(element);
+            element.remove()
+        })
+    })
+}
+
+function removeBook(element){
+    const index = element.classList[1].slice(-1)
+    myLibrary.splice(index, 1)
+}
+
+function addToggleRead(btnRead){
+    btnRead.forEach((value) => {
+        value.addEventListener("click", (event) => {
+            parentElement = event.target.parentElement.parentElement
+            element = parentElement.querySelector('.is-read')
+            toggleRead(element);
+        })
+    })
+}
+
+function toggleRead(element){
+    if (element.textContent === 'Not Read') {
+        element.textContent = 'Read'
+    }
+    else if (element.textContent === 'Read') {
+        element.textContent = 'Not Read'
+    }
+}
